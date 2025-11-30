cd444814d0a015aa464892f2b4930824225755ee
shalomsimon-sketch <shalom.simon@binghamuni.edu.ng>
2025-11-30 13:36:32 -0800

    Add files via upload

---
+++ physics formula Cos.py
@@ -0,0 +1,13 @@
+print("........welcome to formula chooser.......")
+print('1. formula A\n2, formula B\n3, formula C\n4, formula D')
+choose = input('choose: a formula from(1-4)')
+if choose == '1':
+    print('1. you are now using formula A')
+elif choose == '2':
+    print('2. you are now using formula B')
+elif choose == '3':
+    print('3. you are now using formula C')
+elif choose == '4':
+    print('4. you are now using formula D')
+else:
+    print('invalid choice')
